9e710188da7a8ff337bbf44d9ef1e35a309c8af6
ssanico <47161118+in-Sanico2019@users.noreply.github.com>
2019-05-29 00:15:20 -0400

    Revert "Jay's Fix"
    This reverts commit bc1e7d4fe44413d90dfb7881e1bfe0e4792670fa.

--- Prototype_Combat/Source/Prototype_Combat/Public/GameplayController.h
+++ Prototype_Combat/Source/Prototype_Combat/Public/GameplayController.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
-#include "../FP_FirstPerson/FP_FirstPersonCharacter.h"
+#include "FP_FirstPersonCharacter.h"
 #include "GameplayController.generated.h"
 
 /**
